0765a34b63c2e75513ed105d9b0dc0bd313ae74d
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-07-17 16:33:22 -0500

    fixed formatting a bit. added more js

--- Assets/script.js
+++ Assets/script.js
@@ -7,80 +7,89 @@ var forecast = document.getElementById('forecast')
 var cityInfo = document.getElementById('city_info')
 var searchStorage = JSON.parse(localStorage.getItem('prevSearch')) || []
 
-searchHistory();
+// searchHistory();
 
 function getWeather(city) {
-    fetch('api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + apiKey)
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + apiKey)
     .then(function(response) {
         return response.json();
     })
     .then (function (data) {
         // icon
         var imgEl = document.createElement('img')
-        imgEl.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png' 
+        imgEl.src = 'https://openweathermap.org/img/wn/' + data.list[0].weather[0].icon + '.png' 
 
         //name
         var cityEl = document.createElement('h2')
-        cityEl.innerText = data.name
-        current.appendChild(imgEl)
-        current.appendChild(cityEl)
+        cityEl.innerText = data.city.name
+
+        //date
+        var dateEl = document.createElement('p')
+        var date = new Date(data.list[0].dt*1000)
+        dateEl.innerText = date.toDateString()
 
         //temp
         var tempEl = document.createElement('li')
-        tempEl.innerText = 'Temperature (F): ' + data.main.temp 
+        tempEl.innerText = 'Temperature (F): ' + data.list[0].main.temp 
 
         //humidity
         var humidEl = document.createElement('li')
-        humidEl.innerText = 'Humidity (%): ' + data.main.humidity
+        humidEl.innerText = 'Humidity (%): ' + data.list[0].main.humidity
 
         //wind speed
         var windEl = document.createElement('li')
-        windEl.innerText = 'Wind Speed (mph): ' + data.main.humidity
+        windEl.innerText = 'Wind Speed (mph): ' + data.list[0].wind.speed
         
         //append stats to ul
-        cityInfo.appendChild(tempEl)
-        cityInfo.appendChild(humidEl)
-        cityInfo.appendChild(windEl)
-        addSearchHistory(city)
-        searchHistory()
+
+        current.appendChild(imgEl)
+        current.appendChild(cityEl)
+        current.appendChild(dateEl)
+        current.appendChild(tempEl)
+        current.appendChild(humidEl)
+        current.appendChild(windEl)
+
+        // addSearchHistory(city)
+        // searchHistory()
+        weatherData = data
         forecastDisplay()
 
     })
-    .catch (function (error) {
-        console.error('Error code:', error)
-    })
+    // .catch (function (error) {
+    //     console.error('Error code:', error)
+    // })
 }
 
-function forecastDisplay(data) {
-    var arrayEl = data.list
+function forecastDisplay() {
  
-    for(var i = 0; i<arrayEl.length; i+=8 ) {
+    for(var i = 0; i<weatherData.list.length; i+=8 ) {
         // icon
         var imgEl = document.createElement('img')
-        imgEl.src = 'https://openweathermap.org/img/wn/' + arrayEl[i].weather[0].icon + '.png' 
+        imgEl.src = 'https://openweathermap.org/img/wn/' + weatherData.list[i].weather[0].icon + '.png' 
+       
         //Date
-
-        var dateEl = document.createElement('h5')
-        dateEl.innerText = arrayEl[i].dt_txt.split('')[0]
+        var dateEl = document.createElement('p')
+        var date = new Date(weatherData.list[i].dt*1000)
+        dateEl.innerText = date.toDateString()
 
         //temp
         var tempEl = document.createElement('li')
-        tempEl.innerText = 'Temperature (F): ' + arrayEl[i].main.temp 
+        tempEl.innerText = 'Temperature (F): ' + weatherData.list[i].main.temp 
 
         //humidity
         var humidEl = document.createElement('li')
-        humidEl.innerText = 'Humidity (%): ' + arrayEl[i].main.humidity
+        humidEl.innerText = 'Humidity (%): ' + weatherData.list[i].main.humidity
 
         //wind speed
         var windEl = document.createElement('li')
-        windEl.innerText = 'Wind Speed (mph): ' + arrayEl[i].main.humidity
+        windEl.innerText = 'Wind Speed (mph): ' + weatherData.list[i].wind.speed
  
         var forecastCard = document.createElement('div')
         forecastCard.classList.add('forecastCard')
         forecastCard.appendChild(imgEl)
         forecastCard.appendChild(dateEl)
         forecastCard.appendChild(tempEl)
-        forecastCard.appendChild(humidityEl)
+        forecastCard.appendChild(humidEl)
         forecastCard.appendChild(windEl)
         forecast.appendChild(forecastCard)
     }
@@ -105,22 +114,22 @@ searchBtn.addEventListener('click', function(event) {
 //storage functions
 //
 
-function addSearchHistory(input) {
-    searchStorage.push(input) // adds or builds the searchStorage array
-    localStorage.setItem('prevSearch',JSON.stringify(prevSearch))
-}
+// function addSearchHistory(input) {
+//     searchStorage.push(input) // adds or builds the searchStorage array
+//     localStorage.setItem('prevSearch',JSON.stringify(prevSearch))
+// }
 
-function getSearchHistory(prevSearch) {
-    var output = localStorage.getItem(prevSearch)
-    return JSON.parse(output)
-}
+// function getSearchHistory(prevSearch) {
+//     var output = localStorage.getItem(prevSearch)
+//     return JSON.parse(output)
+// }
 
-function searchHistory() {
-    var previousSearches = document.getElementById('search_history')
-    previousSearches.innerHTML = ''
+// function searchHistory() {
+//     var previousSearches = document.getElementById('search_history')
+//     previousSearches.innerHTML = ''
     
-    if (getSearchHistory('prevSearch')) {
-        console.log('okay')
-    }
+//     if (getSearchHistory('prevSearch')) {
+//         console.log('okay')
+//     }
     
-}
+// }
